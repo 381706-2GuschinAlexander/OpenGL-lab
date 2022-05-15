f800f87d532dab89e841e1b090830a5f504be9e0
igush <igush@DESKTOP-3OTTB5S>
2022-05-15 19:25:10 +0300

    custom sphere

--- src/lib/lib.h
+++ src/lib/lib.h
@@ -1,5 +1,84 @@
+#include <GL/glut.h>
+#include <tuple>
+#include <cmath>
 
+void SetMaterialType1(){
+  const GLfloat mat_ambient[] = { 0.7f, 0.7f, 0.7f, 1.0f }; 
+  const GLfloat mat_diffuse[] = { 0.8f, 0.8f, 0.8f, 1.0f }; 
+  const GLfloat mat_specular[] = { .0f, .0f, .0f, 1.0f }; 
+  const GLfloat high_shininess[] = { 100.0f }; 
 
+  glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+  glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+  glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+}
 
+void SetMaterialType2(){
+  const GLfloat mat_ambient[] = { 1.f, 1.f, 1.f, 1.0f }; 
+  const GLfloat mat_diffuse[] = { .0f, .0f, .0f, 1.0f }; 
+  const GLfloat mat_specular[] = { .0f, .0f, .0f, 1.0f }; 
+  const GLfloat high_shininess[] = { 100.0f }; 
 
+  glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+  glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+  glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+  glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+}
 
+void SetLighting1(){
+  const GLfloat light_ambient[] = {0.1f, 0.1f, 0.1f, 1.0f};
+  const GLfloat light_diffuse[] = {0.8f, 0.8f, 0.8f, 1.0f};
+  const GLfloat light_specular[] = {0.5f, 0.5f, 0.5f, 1.0f};
+
+  glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+}
+
+void SetLighting2(){
+  const GLfloat light_ambient[] = {1.f, 1.f, 1.f, 1.0f};
+  const GLfloat light_diffuse[] = {.0f, .0f, .0f, 1.0f};
+  const GLfloat light_specular[] = {.0f, .0f, .0f, 1.0f};
+
+  glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+}
+
+std::tuple<double, double, double> get_xyz(double angle, double r){
+  double rad = angle * M_PI / 180; 
+  return std::make_tuple(r * cos(rad), 0, r * sin(rad));
+}
+
+void ConstructSphere(double r, int x_slices, int y_slices){
+    auto find_xyz = [=](double teta, double phi)->std::tuple<float, float, float>{
+        return std::make_tuple(r * sin(teta) * cos(phi), r*cos(teta), r * sin(teta) * sin(phi));
+    };
+    auto radian = [](double angle){
+        return angle * M_PI / 180;
+    };
+
+ 
+    double x_step = 360.0 / x_slices, y_step = 180.0 / y_slices;
+    double phi_step  = radian(x_step), teta_step  = radian(y_step);
+
+    glBegin(GL_QUADS);
+    for(int  i = 0; i < y_slices; ++i){
+        double cur_teta = teta_step * i;
+        for(int j = 0; j < x_slices; ++j){
+            double cur_phi = phi_step * j;
+
+            auto [x00, y00, z00] = find_xyz(cur_teta, cur_phi);
+            auto [x01, y01, z01] = find_xyz(cur_teta, cur_phi + phi_step);
+            auto [x10, y10, z10] = find_xyz(cur_teta + teta_step, cur_phi);
+            auto [x11, y11, z11] = find_xyz(cur_teta + teta_step, cur_phi + phi_step);
+
+            glNormal3f(x10, y10, z10); glVertex3f(x10, y10, z10);
+            glNormal3f(x00, y00, z00); glVertex3f(x00, y00, z00);
+            glNormal3f(x01, y01, z01); glVertex3f(x01, y01, z01);
+            glNormal3f(x11, y11, z11); glVertex3f(x11, y11, z11);
+        }
+    }
+    glEnd();
+}
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,27 +1,108 @@
-#include <GL/glut.h>
+#include <tuple>
+#include <cmath>
+#include <stdlib.h> 
 #include "lib.h"
+#include <gl\glaux.h>
 
-void Display()
+const GLfloat light_position[] = { .0f, .0f, .0f, 1.0f };
+
+static const int FPS = 60;
+static GLfloat currentAngleOfRotation = 0.0;
+
+static int slices = 16;
+static int stacks = 16;
+static void resize(int width, int height)
 {
-  glClear(GL_COLOR_BUFFER_BIT);
-  glFlush();
+    const float ar = (float)width / (float)height;
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    // gluPerspective(60.0, (GLfloat)width / (GLfloat)height, 1.0, 40.0);
+    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0); // Функция glFrustum умножает текущую матрицу
+    //на перспективную матрицу.
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
+
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+    {
+        glTranslated(0, 0, -10);
+        glRotated(50, 1, 0, 0);
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+        glPushMatrix();
+        {
+            SetLighting1();
+            SetMaterialType1();
+            glColor3d(0, 1, 0);
+            auto [x, y, z] = get_xyz(currentAngleOfRotation, 3);
+            glTranslated(x, y, z);
+            // glRotated(currentAngleOfRotation, 1, 0, 0);
+            ConstructSphere(0.7, slices, stacks);
+            //glutSolidSphere(0.7, slices, stacks);
+        }
+        glPopMatrix();
+
+        glPushMatrix();
+        {
+            SetLighting2();
+            SetMaterialType2();
+            glColor3d(1, 0.6, 0.21);
+            glTranslated(0, 0, 0);
+            glutSolidSphere(0.7, slices, stacks);
+        }
+        glPopMatrix();
+    }
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+
+void timer(int v)
+{
+    if (true)
+    {
+        currentAngleOfRotation += 1.0;
+        if (currentAngleOfRotation > 360.0)
+        {
+            currentAngleOfRotation -= 360.0;
+        }
+        glutPostRedisplay();
+    }
+    glutTimerFunc(1000 / FPS, timer, v);
+}
+
 void Initialize()
 {
-  glClearColor(0.4, 0.4, 0.6, 1.0);                 //Обозначаем цвет фона
-  glMatrixMode(GL_PROJECTION);                      // выбираем тип матрицы
-  glLoadIdentity();                                 // загружаемся с этими настройками
-  glOrtho(-200.0, 200.0, -200.0, 200.0, -5.0, 5.0); // устанавливем диапазон изменения координат
+    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL); // для одного или нескольких параметров материала
+    glEnable(GL_LIGHTING);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glClearColor(0, 0, 0, 1);
+    GLfloat qaLightPosition[] = {0, 0, 1.0, 1.0};
+    glLightfv(GL_LIGHT0, GL_POSITION, qaLightPosition);
 }
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize(400, 400);
-  glutInitWindowPosition(100, 200);
-  glutCreateWindow("Our first GLUT application!");
-  glutDisplayFunc(Display);
-  Initialize();
-  glutMainLoop();
-  return 0;
+    glutInit(&argc, argv);
+    glutInitWindowSize(1000, 480);
+    glutInitWindowPosition(150, 150);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("GLUT Shapes");
+
+    glutReshapeFunc(resize);
+    glutDisplayFunc(display);
+    // 9auxDIBImageLoad();
+    glutTimerFunc(100, timer, 0);
+    Initialize();
+    glutMainLoop();
+    return 0;
 }
